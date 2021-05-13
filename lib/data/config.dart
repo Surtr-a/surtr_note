0d42686326f66ab47b07df1e9bb95672e1755538
Surtr-a <35805398+Surtr-a@users.noreply.github.com>
2021-05-13 10:59:00 +0800

    Update config.dart

--- lib/data/config.dart
+++ lib/data/config.dart
@@ -1,6 +1,6 @@
 class Config {
   static const String BAIDU_TOKEN_SP_KEY = 'baidu_token_key';
 
-  static const String CLIENT_ID = 'NMGPvsugB65cGo6QLIuSrqef';
-  static const String CLIENT_SECRET = '05x2RIbkqswqNyZKiZzL4ZpKLoFp2i9Y';
+  static const String CLIENT_ID = '';
+  static const String CLIENT_SECRET = '';
 }
